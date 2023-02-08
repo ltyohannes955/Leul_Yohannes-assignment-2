6886eb6c870b4f317de9bc915c89eb3a64967d51
tito yohannes <ltyohannes95@gmail.com>
2023-02-08 18:00:41 +0300

    added the wireframe and ui design

--- lib/main.dart
+++ lib/main.dart
@@ -22,106 +22,104 @@ class ArtApp extends StatelessWidget {
         )
       ],
     ),
-    body: Container(
-      child: ListView(
-        
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: const Text('Recent Arts', 
-            style: TextStyle(fontSize: 40, 
-            fontWeight: FontWeight.bold, 
-            fontStyle: FontStyle.italic),),
-            
-          ),
-          const SizedBox(height: 10,),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 0, 10, 0),
-            child: Row(
-              children: const <Widget>[
-                Image(image: AssetImage('assets/IMG 2.jpg'), width: 150, ),
-                SizedBox(width: 50,),
-                Image(image: AssetImage('assets/IMG 6.jpg'), width: 150, ),
-              ],
-            ),
-          ),
-          Row(
+    body: ListView(
+      
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(20.0),
+          color: Colors.white,
+          child: const Text('Recent Arts', 
+          style: TextStyle(fontSize: 40, 
+          fontWeight: FontWeight.bold, 
+          fontStyle: FontStyle.italic),),
+          
+        ),
+        const SizedBox(height: 10,),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 0, 10, 0),
+          child: Row(
             children: const <Widget>[
-              Padding(
-                padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
-                child: Text('Name:')
-                ),
-                Padding(padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
-                child: Text('"name"'),
-                ),
-                Padding(padding: EdgeInsets.fromLTRB(165,20,0,0),
-                child: Text('Name:'),
-                ),
-                Padding(padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
-                child: Text('"name"'),
-                )
+              Image(image: AssetImage('assets/IMG 2.jpg'), width: 150, ),
+              SizedBox(width: 50,),
+              Image(image: AssetImage('assets/IMG 6.jpg'), width: 150, ),
             ],
           ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 40, 0, 0),
-            child: Row(
-              children: const <Widget>[
-                Image(image: AssetImage('assets/IMG 5.jpg'), width: 150,),
-                SizedBox(width: 50,),
-                Image(image: AssetImage('assets/IMG 3.jpg'), width: 150,),
-              ],
-            ),
-          ),
-           Row(
+        ),
+        Row(
+          children: const <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+              child: Text('Name:')
+              ),
+              Padding(padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
+              child: Text('"name"'),
+              ),
+              Padding(padding: EdgeInsets.fromLTRB(110,20,0,0),
+              child: Text('Name:'),
+              ),
+              Padding(padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
+              child: Text('"name"'),
+              )
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 40, 0, 0),
+          child: Row(
             children: const <Widget>[
-              Padding(
-                padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
-                child: Text('Name:')
-                ),
-                Padding(padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
-                child: Text('"name"'),
-                ),
-                Padding(padding: EdgeInsets.fromLTRB(165,20,0,0),
-                child: Text('Name:'),
-                ),
-                Padding(padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
-                child: Text('"name"'),
-                )
+              Image(image: AssetImage('assets/IMG 5.jpg'), width: 150,),
+              SizedBox(width: 50,),
+              Image(image: AssetImage('assets/IMG 3.jpg'), width: 150,),
             ],
           ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 40, 0, 0),
-            child: Row(
-              children: const <Widget>[
-                Image(image: AssetImage('assets/IMG 2.jpg'), width: 150,),
-                SizedBox(width: 50,),
-                Image(image: AssetImage('assets/IMG 4.jpg'), width: 150,),
-              ],
-            ),
+        ),
+         Row(
+          children: const <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+              child: Text('Name:')
+              ),
+              Padding(padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
+              child: Text('"name"'),
+              ),
+              Padding(padding: EdgeInsets.fromLTRB(110,20,0,0),
+              child: Text('Name:'),
+              ),
+              Padding(padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
+              child: Text('"name"'),
+              )
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 40, 0, 0),
+          child: Row(
+            children: const <Widget>[
+              Image(image: AssetImage('assets/IMG 2.jpg'), width: 150,),
+              SizedBox(width: 50,),
+              Image(image: AssetImage('assets/IMG 4.jpg'), width: 150,),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 40, 0, 0),
-            child: Row(
-              children: const <Widget>[
-                Image(image: AssetImage('assets/IMG 2.jpg'), width: 150,),
-                SizedBox(width: 50,),
-                Image(image: AssetImage('assets/IMG 1.jpg'), width: 150,),
-              ],
-            ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 40, 0, 0),
+          child: Row(
+            children: const <Widget>[
+              Image(image: AssetImage('assets/IMG 2.jpg'), width: 150,),
+              SizedBox(width: 50,),
+              Image(image: AssetImage('assets/IMG 1.jpg'), width: 150,),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 40, 0, 0),
-            child: Row(
-              children: const <Widget>[
-                Image(image: AssetImage('assets/IMG 3.jpg'), width: 150,),
-                SizedBox(width: 50,),
-                Image(image: AssetImage('assets/IMG 5.jpg'), width: 150,),
-              ],
-            ),
-          )
-        ],
-      ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 40, 0, 0),
+          child: Row(
+            children: const <Widget>[
+              Image(image: AssetImage('assets/IMG 3.jpg'), width: 150,),
+              SizedBox(width: 50,),
+              Image(image: AssetImage('assets/IMG 5.jpg'), width: 150,),
+            ],
+          ),
+        )
+      ],
     ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.black,
